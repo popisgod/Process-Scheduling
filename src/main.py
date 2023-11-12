50910253e30f1d4c5a325e6130862cffa447fc9e
popisgod <ronglozman2006@gmail.com>
2023-11-13 01:56:00 +0200

    Revert "created new file for interactive scheduling"
    This reverts commit 90e7b86ecdd2bf0c6f9467b19d7a5697c36e947c.
    
    Accidently added .vscode file to the commit

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
-from scheduling_algorithms_batch import FCFS, SJF
+from scheduling_algorithms import FCFS, SJF
 from process import Process
 
 
 
 
 if __name__ == "__main__":
+    
     # Create some processes
     processes = [Process.create_random_process(i) for i in range(15)]
 
     # Execute the FCFS algorithm
     FCFS(processes)
-
+    
     # Execute the SJF algorithm
     SJF(processes)
